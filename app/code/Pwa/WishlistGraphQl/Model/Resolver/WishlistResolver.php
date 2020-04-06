45d92b4a1b7387e0f8c09d923888f69fa4e27e08
caesar <454451758@qq.com>
2020-04-06 09:06:38 +0800

    Update WishlistGraphQl Modules

---
+++ app/code/Pwa/WishlistGraphQl/Model/Resolver/WishlistResolver.php
@@ -0,0 +1,68 @@
+<?php
+declare(strict_types=1);
+
+namespace Pwa\WishlistGraphQl\Model\Resolver;
+
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Wishlist\Model\ResourceModel\Wishlist as WishlistResourceModel;
+use Magento\Wishlist\Model\Wishlist;
+use Magento\Wishlist\Model\WishlistFactory;
+
+/**
+ * Fetches the Wishlist data according to the GraphQL schema
+ */
+class WishlistResolver implements ResolverInterface
+{
+    /**
+     * @var WishlistResourceModel
+     */
+    private $wishlistResource;
+
+    /**
+     * @var WishlistFactory
+     */
+    private $wishlistFactory;
+
+    /**
+     * @param WishlistResourceModel $wishlistResource
+     * @param WishlistFactory $wishlistFactory
+     */
+    public function __construct(WishlistResourceModel $wishlistResource, WishlistFactory $wishlistFactory)
+    {
+        $this->wishlistResource = $wishlistResource;
+        $this->wishlistFactory = $wishlistFactory;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ) {
+        $customerId = $context->getUserId();
+
+        /** @var Wishlist $wishlist */
+        $wishlist = $this->wishlistFactory->create();
+        $this->wishlistResource->load($wishlist, $customerId, 'customer_id');
+
+        if (!$wishlist->getId()) {
+            return [
+                'model' => $wishlist,
+            ];
+        }
+
+        return [
+            'sharing_code' => $wishlist->getSharingCode(),
+            'updated_at' => $wishlist->getUpdatedAt(),
+            'items_count' => $wishlist->getItemsCount(),
+            'name' => $wishlist->getName(),
+            'model' => $wishlist,
+        ];
+    }
+}
